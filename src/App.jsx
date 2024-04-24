28612481ef24cd04604f6b244faaf4ff99f4fc89
UdayGodaba <godabauday@gmail.com>
2024-04-24 13:19:26 +0530

    formConfig structure fix and basic form selection done

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,37 @@
 import "./App.css";
 import Form from "./components/Form";
-import { studentForm, feedbackForm, contactForm } from "./utils/formConfig";
+import { forms } from "./utils/formConfig";
+
+import { useState } from "react";
+import Select from "@mui/material/Select";
+import MenuItem from "@mui/material/MenuItem";
+import Grid from "@mui/material/Grid";
+import Typography from "@mui/material/Typography";
 
 function App() {
+  const [formType, setFormType] = useState("Contact Form");
+  const handleChange = (e) => setFormType(e.target.value);
+
   return (
-    <>
-      <Form formConfig={contactForm} />
-    </>
+    <Grid container spacing={3} sx={{ padding: 5 }}>
+      <Grid item xs={12}>
+        <Select value={formType} onChange={handleChange}>
+          {Object.keys(forms).map((formKey, idx) => (
+            <MenuItem value={formKey} key={idx}>
+              {formKey}
+            </MenuItem>
+          ))}
+        </Select>
+      </Grid>
+      <Grid item xs={12}>
+        <Typography variant="h6" fontWeight={900}>
+          {formType.toUpperCase()}
+        </Typography>
+      </Grid>
+      <Grid item xs={12}>
+        <Form formConfig={forms[formType]} />
+      </Grid>
+    </Grid>
   );
 }
 
--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,7 +1,20 @@
 /* eslint-disable react/prop-types */
+import Grid from "@mui/material/Grid";
+import Typography from "@mui/material/Typography";
+
 const Form = ({ formConfig }) => {
   console.log(formConfig);
-  return <div>Form</div>;
+  return (
+    <from>
+      <Grid container>
+        {formConfig.map((element, idx) => (
+          <Grid item xs={12} key={idx}>
+            <Typography variant="subtitle1">{element.label}</Typography>
+          </Grid>
+        ))}
+      </Grid>
+    </from>
+  );
 };
 
 export default Form;
